6751fbb92a1d2a6fcd2087b3db8af215c94c7d4b
vithusan99 <vithusan091999@gmail.com>
2022-10-03 12:28:25 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import './App.css';
-import Dashboard from './pages/Dashboard';
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Agr from "./components/Agr";
+import Agr2 from "./components/Agr2";
+import AgrForm from "./components/Agr_form";
+import AreaFeedback from "./components/AreaFeedback";
+import CommonDash from "./components/CommonDash";
+import FeedbackCleint from "./components/FeedbackCleint";
+import SelectPlace from "./components/SelectPlace";
+import Vcm01 from "./components/Vcm01";
+import Vs01 from "./components/Vs01";
+import Vs02 from "./components/Vs02";
+import WeatherForacst from "./components/weatherForacst";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App h-100">
-      <Dashboard/>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/frame1" />} />
+        <Route path="/" element={<Dashboard />}>
+          <Route path="frame1" element={<Vcm01 />} />
+          <Route path="frame2" element={<Vs01 />} />
+          <Route path="frame3" element={<Vs02 />} />
+          <Route path="frame4" element={<Agr />} />
+          <Route path="frame5" element={<Agr2 />} />
+          <Route path="frame6" element={<AgrForm />} />
+          <Route path="frame7" element={<WeatherForacst />} />
+          <Route path="frame8" element={<CommonDash />} />
+          <Route path="frame9" element={<SelectPlace />} />
+          <Route path="frame10" element={<AreaFeedback />} />
+          <Route path="frame11" element={<FeedbackCleint />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,19 +1,7 @@
 import React from 'react'
-import Feedback from 'react-bootstrap/esm/Feedback'
-import { Route, Routes, Redirect, Navigate } from 'react-router-dom'
-import Agr from '../components/Agr'
-import Agr2 from '../components/Agr2'
-import AgrForm from '../components/Agr_form'
-import AreaFeedback from '../components/AreaFeedback'
-import CommonDash from '../components/CommonDash'
-import FeedbackCleint from '../components/FeedbackCleint'
+import { Outlet} from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import SelectPlace from '../components/SelectPlace'
 import Sidebar from '../components/Sidebar'
-import Vcm01 from '../components/Vcm01'
-import Vs01 from '../components/Vs01'
-import Vs02 from '../components/Vs02'
-import WeatherForacst from '../components/weatherForacst'
 
 function Dashboard() {
   return (
@@ -23,20 +11,7 @@ function Dashboard() {
         </div>
         <div className=' w-100  main_board  pt-3'>
             <Navbar />
-            <Routes>
-              <Route  path="/" element={<Navigate replace to="/frame1"/>}/>
-              <Route path="/frame1" element={<Vcm01 />}/>
-              <Route path="/frame2" element={<Vs01 />}/>
-              <Route path="/frame3" element={<Vs02 />}/>
-              <Route path="/frame4" element={<Agr />}/>
-              <Route path="/frame5" element={<Agr2 />}/>
-              <Route path="/frame6" element={<AgrForm/>}/>
-              <Route path="/frame7" element={<WeatherForacst/>}/>
-              <Route path="/frame8" element={<CommonDash/>}/>
-              <Route path="/frame9" element={<SelectPlace/>}/>
-              <Route path="/frame10" element={<AreaFeedback/>}/>
-              <Route path="/frame11" element={<FeedbackCleint/>}/>
-            </Routes>
+           <Outlet/>
         </div>
     </div>
   )
